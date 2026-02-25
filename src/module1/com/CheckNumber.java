2435d3a8d2a9462e2e9a30f89709328590e2d48d
DADAPEER786786 <dadapeerirfan97@gmail.com>
2026-02-25 13:06:32 +0530

    Initial commit

---
+++ src/module1/com/CheckNumber.java
@@ -0,0 +1,19 @@
+package module1.com;
+
+import java.util.Scanner;
+
+public class CheckNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println(num+" is Positive number");
+        } else if (num < 0) {
+            System.out.println(num+" is Negative number");
+        } else {
+            System.out.println(num+" is Zero");
+        }
+    }
+}
---
+++ src/module2/com/FactorialRecursion.java
@@ -0,0 +1,18 @@
+package module2.com;
+import java.util.*;
+public class FactorialRecursion {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Factorial: " + factorial(num));
+    }
+}
