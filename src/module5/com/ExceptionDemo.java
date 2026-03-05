153029fc927d8fe95af91f0cc3c2e4ac70e3e49c
DADAPEER786786 <dadapeerirfan97@gmail.com>
2026-03-05 08:20:51 +0530

    Assignment 2

---
+++ src/module5/com/ExceptionDemo.java
@@ -0,0 +1,20 @@
+package module5.com;
+import java.io.*;
+
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            FileReader file = new FileReader("abc.txt");
+        } catch (IOException e) {
+            System.out.println("Checked Exception handled");
+        }
+
+        try {
+            int a = 10/0;
+        } catch (ArithmeticException e) {
+            System.out.println("Unchecked Exception handled");
+        }
+    }
+}
